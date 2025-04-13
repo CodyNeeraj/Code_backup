1e51a4573154c6513b3600a464c04293a66ff2c5
Neeraj <60183278+CodyNeeraj@users.noreply.github.com>
2025-04-13 16:50:47 +0530

    Update and rename Binding.js to Binding or without polyfill.js

--- CodeFromSeleniumCrawler/Binding or without polyfill.js
+++ CodeFromSeleniumCrawler/Binding or without polyfill.js
@@ -8,16 +8,18 @@ let printname = function (testparam,testparam2,testparam3){
 }
 
 let printname1 = printname.bind(name)
+// here printname is internally returning a function so do we've to do the same in our custom function
+// printname1()
+
+
+//here this keyword will point out to the function definition of the
+// called on function on which custom bind property is applied
+// which is printname here !!
 
 Function.prototype.newBindMethod = function (...args){
     let obj = this
+    console.log("from custom bind method "+ this)
     return function (){
         obj.call(args[0])
     }
-}
-
-Function.prototype.newBindMethod = function (...args){
-    let obj = this
-    let params = args.slice(1)
-    console.log("Inside the main method ",params)
 }
